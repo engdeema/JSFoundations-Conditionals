35c7d0076b21847f7386c7652cfbe9f0e0f39363
Deema Burhama <eng.dburhama@gmail.com>
2021-09-21 22:33:56 +0300

    bravo

--- calculator.js
+++ calculator.js
@@ -4,3 +4,30 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 // Run the code to see how to use prompt.
 const name = prompt("What's your name? ");
 console.log(`Hi, I'm ${name}.`);
+
+let firstNumber = prompt(`Please enter the first number`);
+let secondNumber = prompt(`Please enter the second number`);
+let operation = prompt(`Please choose the operation (+, -, /, *): *`);
+
+firstNumber = +firstNumber;
+secondNumber = +secondNumber;
+
+if (isNaN(firstNumber) && isNaN(secondNumber)) {
+  console.log(" the numbers were invalid");
+} else {
+  if (operation === "+") {
+    let result = firstNumber + secondNumber;
+    console.log(`${firstNumber} + ${secondNumber}= ${result}`);
+  } else if (operation === "-") {
+    let result = firstNumber - secondNumber;
+    console.log(`${firstNumber} - ${secondNumber}= ${result}`);
+  } else if (operation === "*") {
+    let result = firstNumber * secondNumber;
+    console.log(`${firstNumber} * ${secondNumber}= ${result}`);
+  } else if (operation === "%") {
+    let result = firstNumber % secondNumber;
+    console.log(`${firstNumber} % ${secondNumber}= ${result}`);
+  } else {
+    console.log("operation is not valid");
+  }
+}
